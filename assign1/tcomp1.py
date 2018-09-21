72c00884766961b3fe6d830bf335ab83c3f110d3
Stephen Pollett <14841823+stephen-64@users.noreply.github.com>
2018-09-21 15:26:53 -0230

    Initial commit

---
+++ assign1/tcomp1.py
@@ -0,0 +1,146 @@
+'''
+This script process the files given as command line arguments and extracts
+the n-grams stored within them using the given value n as the type of gram
+ex: n=2 will look for 2-grams
+usage: python tcomp1.py filename n file1name file2name ...
+'''
+import sys
+import io
+
+'''
+An instance of this class is created for every file
+passed as a cmd line argument to the program. 
+This class will hold the file,it's name and create the dictionary containing it's
+n-grams
+'''
+class nfile(): 
+
+    '''
+    Initilizases the class and calls get_grams() to calculate the n-grams 
+    '''
+    def __init__(self,fname,ng):
+        self.ngrams={}
+        self.ngrams_list=[]
+        self.total_grams=0.0
+        self.name = fname
+        self.f = open(fname)
+        self.ngrm = ng
+        self.contents = self.f.read()
+        self.get_grams()
+
+    '''
+    Calculates the n-grams contained within this file and stores them in a list
+    '''
+    def get_grams(self):
+        counter = 0
+        for char in self.contents:
+            ngram = char
+            seccount = 1
+            while(char!='\n' and char !=' ' and (seccount<self.ngrm) and ((seccount+counter)<len(self.contents)) and (self.contents[seccount+counter] != " ") and (self.contents[seccount+counter] != "\n")): 
+                ngram+=self.contents[seccount+counter]
+                seccount+=1
+            if((len(ngram))==self.ngrm):
+                self.ngrams_list.append(ngram)
+            counter+=1
+        self.get_grams_dict()
+    
+    '''
+    Converts the list of n-grams into the dictionary required by the assignment 
+    which will be used in the final processing
+    '''
+    def get_grams_dict(self):
+        for st in self.ngrams_list:
+            if(not self.ngrams.has_key(st)):
+                self.ngrams[st] = 1
+                self.total_grams+=1
+            else:
+                self.ngrams[st]+=1
+                self.total_grams+=1
+        for x in self.ngrams:
+            self.ngrams[x]/=self.total_grams
+            
+            
+
+'''
+This call as it's names implies proccesses and creates the nfiles required to 
+perform the processing and find the value for Sim
+'''
+class file_processor():
+
+    '''
+    Parses through the gvien cmd line input and will create the required nfiles and 
+    variables to process the files
+    '''
+    def __init__(self,args):
+        self.flist =[]
+        count = 1
+        if(len(args) < 4):
+            print 'usage: python tcomp1.py filename n file1name file2name ...'
+            exit(1)
+        self.n = int(args[2])
+        for x in args[1:]:
+            if(count == 2):
+                self.n=int(x)
+                count+=1
+            else:
+                self.flist.append(nfile(x,self.n))
+                count+=1
+        del count
+
+    '''
+    Implements the Diff function described in the assignments
+    program spec using a dictionary for n-gram calculations
+    '''
+    def Diff(self,x,y):
+        counter = 0.0
+        for val in x.ngrams:
+            if(y.ngrams.has_key(val)):
+                counter += abs(x.ngrams[val]-y.ngrams[val])
+            else:
+                counter += x.ngrams[val]
+        for val in y.ngrams:
+            if(not x.ngrams.has_key(val)):
+                counter += y.ngrams[val]
+        return counter
+
+    '''
+    Performs the final Sim calculations and 
+    returns the vaules as a float
+    '''
+    def Sim(self,x,y):
+        return 1.0-(self.Diff(self.flist[x],self.flist[y])/2.0)
+
+    '''
+    The main running point of the program which processes the files and 
+    produces the formatted data output
+    '''
+    def run_sim(self):
+        totals={}
+        for x in range(1,len(self.flist)):
+            print 'Sim("' + str(self.flist[0].name) + '","' + str(self.flist[x].name)+'") = '+str(round(self.Sim(0,x),3))
+            totals[self.flist[x].name]=round(self.Sim(0,x),3)
+        total=0.0
+        name=''
+        for x in totals:
+            if totals[x]>total:
+                total=totals[x]
+                name=x
+        print 'File "'+ name+'" is most similar to file "'+self.flist[0].name+'"'
+
+    '''
+    Closes all open files
+    '''
+    def done_files(self):
+        for file in self.flist:
+            file.f.close()
+
+            
+
+def main():
+    fp = file_processor(sys.argv)
+    fp.run_sim()
+    fp.done_files()
+
+
+if(__name__=='__main__'):
+    main()
---
+++ assign1/tcomp2.py
@@ -0,0 +1,131 @@
+'''
+This script process the files given as command line arguments and extracts
+the word count within the file and them compares them outputting the value to the
+terminal
+usage: python tcomp2.py filename file1name file2name ...
+'''
+import sys
+import io
+
+'''
+This class will hadle everything to do with the files 
+storing them as word_files which contain all tools nedded to manage the files
+'''
+class file_handler():
+    
+    '''
+    Sorts through the given command line inputs and stores them as word_files
+    containing all the data required to work with them
+    '''
+    def __init__(self):
+        self.flist = []
+        if(len(sys.argv)<3):
+            print 'usage: python tcomp2.py filename file1name file2name ...'
+            exit(1)
+        for arg in sys.argv[1:]:
+            self.flist.append(word_files(arg))
+
+    '''
+    Closes the open files when done with them
+    '''
+    def close_files(self):
+        for fi in self.flist:
+            fi.close_file()
+
+    '''
+    Runs the defined Sim adding the sums together as expected by the program spec
+    returning a float representing the similarity
+    '''
+    def Sim(self,x,y):
+        nWx=self.flist[x].num_words
+        nWy=self.flist[y].num_words
+        SD = self.SD(self.flist[x].word_list,self.flist[y].word_list)
+        return 1.0 - (SD/(nWx+nWy))
+
+    '''
+    Checks the given values and looks for the differnece in words
+    within them returning a modified sum contianing the difference
+    '''
+    def SD(self,x,y):
+        total_count = 0.0
+        for xnum in x:
+            if xnum not in y:
+                total_count+=1.0
+        for ynum in y:
+            if ynum not in y:
+                total_count+=1.0
+        return total_count
+
+    '''
+    Runs the program properly formating the output as 
+    per the given program spec
+    '''
+    def run_sim(self):
+        totals = {}
+        for num in range(1,len(self.flist)):
+            print 'Sim("' + str(self.flist[0].name) + '","' + str(self.flist[num].name)+'") = '+str(round(self.Sim(0,num),3))
+            totals[self.flist[num].name]=round(self.Sim(0,num),3)
+        total=0.0
+        name=''
+        for x in totals:
+            if totals[x]>total:
+                total=totals[x]
+                name=x
+        print 'File "'+ name+'" is most similar to file "'+self.flist[0].name+'"'
+
+
+
+'''
+Holds the files given as command line arguments and looks for the 
+words placing them in a set as specified
+'''
+class word_files():
+
+    '''
+    Initializes the class storing the file and it's name
+    along with it's word count using get_word_list
+    '''
+    def __init__(self,fname):
+        self.name = fname
+        self.file = open(fname)
+        self.contents = self.file.read()
+        self.word_list=set()
+        self.get_word_list()
+        
+    '''
+    Closes the open file
+    '''
+    def close_file(self):
+        self.file.close()
+
+    '''
+    Genertates the set of all words in the given 
+    file and stores them to a class variabl
+    '''
+    def get_word_list(self):
+        paragraph_list=self.contents.splitlines()
+        word_holder=[]
+        for st in paragraph_list:
+            word_holder.append(st.split(' '))
+        for cont in word_holder:
+            for stri in cont:
+                self.word_list.add(stri)
+                if '' in self.word_list:
+                    self.word_list.remove('')
+                if ' ' in self.word_list:
+                    self.word_list.remove(' ')
+
+        self.num_words=float(len(self.word_list))
+
+
+
+
+def main():
+    fh = file_handler()
+    fh.run_sim()
+    fh.close_files()
+
+if(__name__=='__main__'):
+    main()
+
+    
